64caa06f1c9dcfd7b2ee9554ea1b6bdbf654ce32
Don Vo <holycrapitsachicken@gmail.com>
2018-10-07 01:40:31 -0400

    New Timer

--- DodgeballDeathmatch/Assets/Tests/Editor/TimerTest.cs
+++ DodgeballDeathmatch/Assets/Tests/Editor/TimerTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 [TestFixture]
 public class TimerTest
@@ -8,13 +7,13 @@ public class TimerTest
     private object time;
 
     [Test]
-	public void startTimer()
+    public void startTimer()
     {
         var timer = new Timer();
         timer.setTime(10);
-        timer.start();
+        timer.Start();
 
-        Assert.Greater(10, timer.time);
+        Assert.Greater(10.0, timer.getTime());
     }
 
     [Test]
@@ -23,76 +22,23 @@ public class TimerTest
         var timer = new Timer();
 
         timer.setTime(10);
-        timer.start();
+        timer.Start();
         timer.stop();
 
-        Assert.False(timer.countdown);
-    }
-
-    [Test]
-    public void displayTimer()
-    {
-        var timer = new Timer();
-
-        timer.display(true);
-        Assert.True(timer.visible);
-
-        timer.display(false);
-        Assert.False(condition: timer.visible);
+        Assert.False(timer.isCountingDown());
     }
 
     [Test]
     public void outOfTime()
     {
-        var timer = new TimerTest();
+        var timer = new Timer();
 
-        timer.setTime(1);
-        timer.start();
+        timer.setTime(1.0f);
+        timer.Start();
 
         System.Threading.Thread.Sleep(1000);
 
-        Assert.False(timer.countdown);
-        Assert.Equals(timer.time, 0);
-    }
-
-    private void start()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void setTime(int v)
-    {
-        throw new NotImplementedException();
-    }
-
-    private class Timer
-    {
-        internal int time;
-        internal bool? countdown;
-        internal bool? visible;
-
-        public Timer()
-        {
-        }
-
-        internal void display(bool v)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal void setTime(int v)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal void start()
-        {
-            throw new NotImplementedException();
-        }
-
-        internal void stop()
-        {
-            throw new NotImplementedException();
-        }
+        Assert.False(timer.isCountingDown());
+        Assert.Equals(timer.getTime(), 0.0);
     }
 }
