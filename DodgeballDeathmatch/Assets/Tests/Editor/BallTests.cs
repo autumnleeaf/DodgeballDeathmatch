36405bcb2ba9324fc86f43fa49e9c7f16910c22c
Steve <steve@scldrn.com>
2018-10-08 19:50:43 -0400

    Added Ball default throw test

---
+++ DodgeballDeathmatch/Assets/Tests/Editor/BallTests.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEditor;
+
+public class BallTests {
+
+    [Test]
+    public void Ball_Moves_Right_On_Default_Throw() {
+        var ballPrefab = Resources.Load("Prefabs/Dodgeball");
+        var _dodgeball = PrefabUtility.InstantiatePrefab(ballPrefab) as GameObject;
+        _dodgeball.transform.position = new Vector3(0, 0, 0);
+
+        _dodgeball.GetComponent<BallController>().Throw();
+
+        var velocity = _dodgeball.GetComponent<BallController>().rb.velocity;
+
+        Assert.Greater(velocity.x, 0);
+    }
+}
