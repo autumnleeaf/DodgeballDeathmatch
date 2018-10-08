3dd8cff9d8416218621e25ffe3fe1780ebbd2e30
Steve <steve@scldrn.com>
2018-10-08 19:52:10 -0400

    Added ball moves left on -1 argument

--- DodgeballDeathmatch/Assets/Tests/Editor/BallTests.cs
+++ DodgeballDeathmatch/Assets/Tests/Editor/BallTests.cs
@@ -18,4 +18,18 @@ public class BallTests {
 
         Assert.Greater(velocity.x, 0);
     }
+
+    [Test]
+    public void Ball_Moves_Left_On_NegOne_Direction_Throw()
+    {
+        var ballPrefab = Resources.Load("Prefabs/Dodgeball");
+        var _dodgeball = PrefabUtility.InstantiatePrefab(ballPrefab) as GameObject;
+        _dodgeball.transform.position = new Vector3(0, 0, 0);
+
+        _dodgeball.GetComponent<BallController>().Throw(-1);
+
+        var velocity = _dodgeball.GetComponent<BallController>().rb.velocity;
+
+        Assert.Less(velocity.x, 0);
+    }
 }
