e16953c672954c9cdd296954bdebdb90b1d8fd18
Fejera <Alyssa.Fejer@icloud.com>
2018-11-06 21:22:49 -0500

    Added bottom boundary for knives not to cross
    Updated Knife Script and added lower boundary to dodgeball court wall so that the knife is destroyed if the player is able to avoid it. This also ensures the kinfe does not fall into oblivion.

--- DodgeballDeathmatch/Assets/Scripts/KnifeScript.cs
+++ DodgeballDeathmatch/Assets/Scripts/KnifeScript.cs
@@ -20,10 +20,16 @@ public class KnifeScript : MonoBehaviour {
     // player game object.
     void OnTriggerEnter2D(Collider2D collision)
     {
+        // Statement to decrease player score if it collides with the knife
         if (collision.gameObject.tag == "Player")
         {
             //Decrement Score
 
+            Destroy(gameObject);
+        }
+        // Statement to destory the knife if the player avoids it
+        else if (collision.gameObject.tag == "Boundary")
+        {
             Destroy(gameObject);
         }
     }
