a7f265452cc3204e2ade9e6451a3a9f68b93d12e
Fejera <Alyssa.Fejer@icloud.com>
2018-11-06 21:36:13 -0500

    Added a Random Spawner Script for the Knife
    Attached the Knife prefab to a random knife spawner. Placed the spawner at the top of the dodgeball court.

---
+++ DodgeballDeathmatch/Assets/Scripts/KnifeSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeSpawner : MonoBehaviour 
+{
+    [SerializeField]
+    float maxX;
+
+    public GameObject knife;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    // FUnction to randomly spawn knives throughout the dodgeball court
+    void SpawnKnife()
+    {
+        Instantiate(knife, transform.position, transform.rotation);
+    }
+}
