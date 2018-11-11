d53f54c527b671b050a4d7f8649c6663e436525a
Fejera <Alyssa.Fejer@icloud.com>
2018-11-10 20:03:22 -0500

    Created Game Manager Script to track the game status
    Game Manager script is called when a players health is dimished.

---
+++ DodgeballDeathmatch/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    public void GameOver(int team)
+    {
+        print("GameOver()");
+        print(team);
+
+        if (team == 0)
+        {
+            print("Both Lose");
+        }
+        else if (team == 1)
+        {
+            print("Player 2 Wins");
+        }
+        else if (team == 2)
+        {
+            print("Player 1 Wins");
+        }
+    }
+
+
+}
