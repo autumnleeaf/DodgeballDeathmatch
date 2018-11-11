dda646ce698eb858a25c2cc035c15ad4f709e81f
esteban pizarro <estebanpiza1@knights.ucf.edu>
2018-11-11 17:57:20 -0500

    code for controller added

--- DodgeballDeathmatch/Assets/Scripts/PlayerController.cs
+++ DodgeballDeathmatch/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 using System;
 using UnityEngine.UI;
 using DodgeballDeathmatch;
+using System.IO.Ports;
 
 public class PlayerController : MonoBehaviour
 {
@@ -19,9 +20,11 @@ public class PlayerController : MonoBehaviour
 
     public int health = 100;
     public int team = 1;
+    public int bite ;
 
     private GameObject _dodgeball;
     private Animator myAnimator;
+    static SerialPort sp = new SerialPort("COM3", 9600);
 
     private void Awake()
     {
@@ -32,22 +35,41 @@ public class PlayerController : MonoBehaviour
     {
         myAnimator = GetComponent<Animator>();
         Player = new Player(transform.position, team, _movementSpeed);
+        sp.Open();
+        sp.ReadTimeout = 1;
     }
-    public void Move ()
+    public void Move()
     {
-        Destroy(this.gameObject);
+        if (this != null)
+        {
+            Destroy(this.gameObject);
+        }
+
     }
 
     void Update()
     {
         bool pickupKeyDown = Input.GetKeyDown(Player.PickupKey);
+        bool throwKeyDown = Input.GetKeyDown(Player.ShootKey);
+
+        // If Controller is on assign based on controller
+        if (sp.IsOpen && Player.Team == 2)
+        {
+            bite = sp.ReadByte();
+            if (bite == 1 || pickupKeyDown)
+            {
+                pickupKeyDown = true;
+            }
+            if (bite == 2 || throwKeyDown)
+            {
+                throwKeyDown = true;
+            }
+        }
 
         if (pickupKeyDown && Player.ReachableDodgeballs.Count > 0)
         {
             Player.PickupBall();
         }
-
-        bool throwKeyDown = Input.GetKeyDown(Player.ShootKey);
 
         if (throwKeyDown && Player.BallCount > 0)
         {
@@ -65,6 +87,45 @@ public class PlayerController : MonoBehaviour
     {
         var horizontal = Input.GetAxisRaw(Player.HoriztonalAxisName);
         var vertical = Input.GetAxisRaw(Player.VerticalAxisName);
+
+        // If controller is on, assign based on its inputs
+        if (sp.IsOpen && Player.Team == 2)
+        {
+
+
+
+            switch (sp.ReadByte())
+            {
+                case 13: // up
+                    vertical = 1;
+                    break;
+                case 12: // down
+                    vertical = -1;
+                    break;
+                case 31: // right
+                    horizontal = 1;
+                    break;
+                case 21: // left
+                    horizontal = -1;
+                    break;
+                case 23: //left + up
+                    horizontal = -1;
+                    vertical = 1;
+                    break;
+                case 22: //left + down
+                    horizontal = -1;
+                    vertical = -1;
+                    break;
+                case 33: //right + up
+                    horizontal = 1;
+                    vertical = 1;
+                    break;
+                case 32: //right + down
+                    horizontal = 1;
+                    vertical = -1;
+                    break;
+            }
+        }
 
         // Allows the speed component in the animation editor to see player speed
         myAnimator.SetFloat(animWord, Mathf.Abs(horizontal + vertical));
@@ -105,7 +166,7 @@ public class PlayerController : MonoBehaviour
 
             var _ballController = dodgeball.GetComponent<BallController>();
 
-            if(_ballController.LiveStatus)
+            if (_ballController.LiveStatus)
             {
                 Player.TakeDamage(_ballController.Damage);
 
